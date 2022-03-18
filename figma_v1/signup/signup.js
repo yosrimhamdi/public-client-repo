765bef2545cd47f464b5b0e997b821d76c54dfb1
yosrimhamdi <yosri@mhamdi.co>
2022-03-18 21:30:31 +0100

    Added password toggle functionality.

---
+++ figma_v1/signup/signup.js
@@ -0,0 +1,29 @@
+const eyeIcons = document.querySelectorAll('.eye-icon');
+
+const toggleIcon = eyeIcon => {
+    const src = eyeIcon.getAttribute('src');
+
+    if (src.endsWith('eye-solid.svg')) {
+        eyeIcon.setAttribute('src', '../eye-slash-solid.svg');
+    } else {
+        eyeIcon.setAttribute('src', '../eye-solid.svg');
+    }
+};
+
+const togglePasswordVisibility = eyeIcon => {
+    const passwordInput = eyeIcon.previousElementSibling;
+
+    if (passwordInput.getAttribute('type') === 'password') {
+        passwordInput.setAttribute('type', 'text');
+    } else {
+        passwordInput.setAttribute('type', 'password');
+    }
+};
+
+eyeIcons.forEach(eyeIcon => {
+    eyeIcon.addEventListener('click', () => {
+        toggleIcon(eyeIcon);
+
+        togglePasswordVisibility(eyeIcon);
+    });
+});
