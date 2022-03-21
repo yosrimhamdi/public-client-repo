72f807b784c2f278dfd4499866b17fb17c0c1bc3
Yosri Mhamdi <yosri@cosavostra.com>
2022-03-21 23:11:12 +0100

    Toggling the menu overlay.

---
+++ figma_v1/assets/js/toggle-mobile-menu.js
@@ -0,0 +1,21 @@
+class ToggleMobileMenu {
+  constructor() {
+    this.openIcon = document.querySelector('.mobile-header__bars');
+    this.closeIcon = document.querySelector('.menu-overlay__close-icon');
+    this.menuOverlay = document.querySelector('.menu-overlay');
+
+    this.setEvents();
+  }
+
+  setEvents() {
+    this.openIcon.onclick = () => {
+      this.menuOverlay.classList.add('menu-overlay--visible');
+    };
+
+    this.closeIcon.onclick = () => {
+      this.menuOverlay.classList.remove('menu-overlay--visible');
+    };
+  }
+}
+
+new ToggleMobileMenu();
