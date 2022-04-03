a59864e284819717c1eb6b4b17211d418673c98b
Yosri Mhamdi <yosri@cosavostra.com>
2022-04-04 00:36:12 +0100

    Removed extra js generated file along side with css file.

--- figma_v1/webpack.config.js
+++ figma_v1/webpack.config.js
@@ -1,18 +1,20 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 module.exports = {
-  entry: [
-    '/src/sass/pages/index.sass',
-    '/src/sass/pages/dashboard.sass',
-    '/src/sass/pages/email.sass',
-  ],
+  entry: {
+    index: '/src/sass/pages/index.sass',
+    dashboard: '/src/sass/pages/dashboard.sass',
+    email: '/src/sass/pages/email.sass',
+  },
   output: {
     path: __dirname + '/dist/',
   },
   mode: 'development',
   plugins: [
+    new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
