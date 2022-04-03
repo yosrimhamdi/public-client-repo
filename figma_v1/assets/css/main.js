9250db722422af1b2d628455ad24596976199c27
Yosri Mhamdi <yosri@cosavostra.com>
2022-04-03 23:30:32 +0100

    [Fix] wrong sass syntax.

--- figma_v1/assets/css/main.js
+++ figma_v1/assets/css/main.js
@@ -8,7 +8,64 @@
  */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/sass/pages/dashboard.sass":
+/*!******************************************!*\
+  !*** ./assets/sass/pages/dashboard.sass ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://figma_v1/./assets/sass/pages/dashboard.sass?");
+
+/***/ }),
+
+/***/ "./assets/sass/pages/email.sass":
+/*!**************************************!*\
+  !*** ./assets/sass/pages/email.sass ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://figma_v1/./assets/sass/pages/email.sass?");
+
+/***/ }),
+
+/***/ "./assets/sass/pages/index.sass":
+/*!**************************************!*\
+  !*** ./assets/sass/pages/index.sass ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://figma_v1/./assets/sass/pages/index.sass?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./assets/sass/pages/index.sass"](0, {}, __webpack_require__);
+/******/ 	__webpack_modules__["./assets/sass/pages/dashboard.sass"](0, {}, __webpack_require__);
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./assets/sass/pages/email.sass"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
