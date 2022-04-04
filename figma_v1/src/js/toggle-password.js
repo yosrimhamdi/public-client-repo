20b5f518e203c6d02fd80aa10cb6f2589a469b22
Yosri Mhamdi <yosri@cosavostra.com>
2022-04-04 01:12:28 +0100

    Changed a dirname.

--- figma_v1/src/js/toggle-password.js
+++ figma_v1/src/js/toggle-password.js
@@ -1,29 +1,29 @@
 const eyeIcons = document.querySelectorAll('.eye-icon');
 
 const toggleIcon = eyeIcon => {
-    const src = eyeIcon.getAttribute('src');
+  const src = eyeIcon.getAttribute('src');
 
-    if (src.endsWith('eye-solid.svg')) {
-        eyeIcon.setAttribute('src', '../assets/images/eye-slash-solid.svg');
-    } else {
-        eyeIcon.setAttribute('src', '../assets/images/eye-solid.svg');
-    }
+  if (src.endsWith('eye-solid.svg')) {
+    eyeIcon.setAttribute('src', '../src/images/eye-slash-solid.svg');
+  } else {
+    eyeIcon.setAttribute('src', '../src/images/eye-solid.svg');
+  }
 };
 
 const togglePasswordVisibility = eyeIcon => {
-    const passwordInput = eyeIcon.previousElementSibling;
+  const passwordInput = eyeIcon.previousElementSibling;
 
-    if (passwordInput.getAttribute('type') === 'password') {
-        passwordInput.setAttribute('type', 'text');
-    } else {
-        passwordInput.setAttribute('type', 'password');
-    }
+  if (passwordInput.getAttribute('type') === 'password') {
+    passwordInput.setAttribute('type', 'text');
+  } else {
+    passwordInput.setAttribute('type', 'password');
+  }
 };
 
 eyeIcons.forEach(eyeIcon => {
-    eyeIcon.addEventListener('click', () => {
-        toggleIcon(eyeIcon);
+  eyeIcon.addEventListener('click', () => {
+    toggleIcon(eyeIcon);
 
-        togglePasswordVisibility(eyeIcon);
-    });
+    togglePasswordVisibility(eyeIcon);
+  });
 });
