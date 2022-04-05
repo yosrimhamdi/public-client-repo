789ed17cf2c3236c4aad7c362a86cae7ef3728df
Yosri Mhamdi <yosri@cosavostra.com>
2022-04-05 23:11:07 +0100

    Added dynamic charts.

--- figma_v1/webpack.config.js
+++ figma_v1/webpack.config.js
@@ -1,29 +1,17 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
   entry: {
     index: '/src/sass/pages/index.sass',
     dashboard: '/src/sass/pages/dashboard.sass',
     email: '/src/sass/pages/email.sass',
+    charts: '/src/js/charts.js',
   },
   output: {
     path: __dirname + '/dist/',
   },
   mode: 'development',
   plugins: [
-    new RemovePlugin({
-      after: {
-        test: [
-          {
-            folder: 'dist/',
-            method: absoluteItemPath => {
-              return new RegExp(/\.js$/, 'm').test(absoluteItemPath);
-            },
-          },
-        ],
-      },
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
