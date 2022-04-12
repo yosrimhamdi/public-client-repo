7aba43690c65af26893461fa92c417724b3f3da0
Yosri Mhamdi <yosri@cosavostra.com>
2022-04-12 14:41:32 +0100

    Added a js component.

---
+++ src/js/carousel.js
@@ -0,0 +1,36 @@
+import 'slick-carousel';
+import $ from 'jquery';
+
+$('.brands').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
