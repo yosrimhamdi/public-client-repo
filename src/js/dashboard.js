b06afe3c0f9ef177a218d6c182e84f27acb93d6d
Yosri Mhamdi <yosri@mhamdi.co>
2022-04-14 15:06:06 +0100

    Added the fixed mobile nav.

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,2 +1,11 @@
 import './dashboard/drop-down';
 import './dashboard/charts';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileNav = document.querySelector('.mobile-fixed-bottom-nav');
+
+  document.body.style.setProperty(
+    '--mobile-nav-height',
+    mobileNav.offsetHeight + 'px'
+  );
+});
