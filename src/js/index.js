087ed0439ff232349a22070e4aeeb092d2530529
Yosri Mhamdi <yosri@mhamdi.co>
2022-04-14 23:52:25 +0100

    [fix] homae page menu overlay breaking changes.

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
-import './common/toggle-mobile-menu';
+import ToggleMobileMenu from './common/toggle-mobile-menu';
 import './home/carousel';
+
+new ToggleMobileMenu('#open', '.mobile-side-bar-overlay');
