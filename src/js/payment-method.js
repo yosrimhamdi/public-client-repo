8f37462e27b184939c37ac56244492ba8976e18a
Yosri Mhamdi <yosri@mhamdi.co>
2022-04-27 00:56:23 +0100

    Radio buttons toggle using pure css.

--- src/js/payment-method.js
+++ src/js/payment-method.js
@@ -22,15 +22,3 @@ $('.carousel').slick({
     },
   ],
 });
-
-const radios = document.querySelectorAll('.payment-method__label');
-
-radios.forEach(radio => {
-  radio.addEventListener('click', () => {
-    radios.forEach(radio => {
-      radio.classList.add('payment-method__label--disabled');
-    });
-
-    radio.classList.remove('payment-method__label--disabled');
-  });
-});
